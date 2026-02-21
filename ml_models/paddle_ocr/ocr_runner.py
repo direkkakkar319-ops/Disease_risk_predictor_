57622d6c39362530ed7b5da4e4bb83d1402ad0a4
Direk Kakkar <direkKakkar319@gmail.com>
2026-02-21 15:41:09 +0530

    working of paddle OCR

--- ml_models/paddle_ocr/ocr_runner.py
+++ ml_models/paddle_ocr/ocr_runner.py
@@ -1,1 +1,11 @@
 from paddle_ocr import PaddleOCR
+ocr  = PaddleOCR(use_angle_cls = True, lang = 'en')
+
+def extract_text_from_report(path):#here will put the image path
+    results = ocr.ocr(path, cls=True)
+    extracted_text = []
+    for line in results:
+        for word_info in line:
+            text = word_info[-1][0]
+            extracted_text.append(text)
+    return "\n".join(extracted_text)
