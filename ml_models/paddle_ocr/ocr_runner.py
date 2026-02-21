582067a240831d83df340c2997afd7add1487f90
Direk Kakkar <direkKakkar319@gmail.com>
2026-02-21 18:31:40 +0530

    fixed the mistakes in paddleOCR working

--- ml_models/paddle_ocr/ocr_runner.py
+++ ml_models/paddle_ocr/ocr_runner.py
@@ -1,9 +1,14 @@
-from paddle_ocr import PaddleOCR
+from paddleocr import PaddleOCR
 ocr  = PaddleOCR(use_angle_cls = True, lang = 'en')
 
 def extract_text_from_report(path):#here will put the image path
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The file at {path} does not exist.")
+
     results = ocr.ocr(path, cls=True)
+
     extracted_text = []
+    
     for line in results:
         for word_info in line:
             text = word_info[-1][0]
