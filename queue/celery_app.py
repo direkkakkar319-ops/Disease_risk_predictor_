1d39a294ad5a7e21fe5d83ad48eeed20b78f7c3d
Direk Kakkar <millioniar1717@gmail.com>
2026-03-19 15:19:35 +0530

    Celery Application

--- queue/celery_app.py
+++ queue/celery_app.py
@@ -17,3 +17,13 @@ Broker and backend URLs
 # Backend-> where celery stores task after completion
 CELERY_BROKER_URL     = os.getenv("CELERY_BROKER_URL",     "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+
+"""
+Celery application
+"""
+celery_app = Celery(
+    "MEDSCAN AI", # name of celery app
+    broker=CELERY_BROKER_URL,
+    backend=CELERY_RESULT_BACKEND,
+    include=["queue.tasks"]# tells where tasks are defined
+)
