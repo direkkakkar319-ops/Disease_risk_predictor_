24b8f6bb00ac265a9cd5e3a73eb43b60b66051c8
Direk Kakkar <millioniar1717@gmail.com>
2026-03-19 15:16:10 +0530

    Broker and Backend URLs

--- queue/celery_app.py
+++ queue/celery_app.py
@@ -1,15 +1,19 @@
 from celery import Celery
 from celery.signals import worker_ready, worker_process_init
+import os
+import logging
 """
 worker_ready-->fires once the entire worker is up
 worker_process_init-->fires onces inside the EACH worker process at startup
 """
-import os
-import logging
 
+"""Sets-up logger for this file so we can print messages to the console"""
 logger = logging.getLogger(__name__)
 
 """
-Read broker / backend from environment (set in .env or docker-compose)
+Broker and backend URLs
 """
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6390/0")
+# Broker -> where celery sends the tasks 
+# Backend-> where celery stores task after completion
+CELERY_BROKER_URL     = os.getenv("CELERY_BROKER_URL",     "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
