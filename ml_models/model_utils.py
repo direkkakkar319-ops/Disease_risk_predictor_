82f85d6c83c745652659cb289c7b279eb4d1a03e
Direk Kakkar <millioniar1717@gmail.com>
2026-03-21 16:15:04 +0530

    Model file Resolution
    Added
    _BASE_DIR,
    _XGBOOST_DIR,
    ALLOWED_REPORT_TYPE

--- ml_models/model_utils.py
+++ ml_models/model_utils.py
@@ -10,3 +10,10 @@ RISK_THRESHOLDS = {
     "high":     0.65,
     "moderate": 0.40
 }
+
+"""
+Model file resolution
+"""
+_BASE_DIR = Path(__file__).parent
+_XGBOOST_DIR = _BASE_DIR/"xgboost"
+ALLOWED_REPORT_TYPE = {"blood", "lipid", "vitaminD", "hormone", "kidney", "liver"}
